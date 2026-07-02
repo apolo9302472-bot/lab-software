0918d039fc3f20c3e4a40b87854d6557179b993c
RafaZenitus <rafazenitus@gmail.com>
2026-07-02 15:18:01 -0300

    Correção Ex01

--- exerciciosderevisao/02-RevisaoJS/ex01.js
+++ exerciciosderevisao/02-RevisaoJS/ex01.js
@@ -5,6 +5,6 @@
 // Estou aprendendo JavaScript
 let nome = "apolo"
 let idade = 1200 
-console.log(typeof nome);
+console.log(typeof nome); // Utilziando "typeof" será imprimido o tipo da variável e não o que está armazenado dela 
 console.log(typeof idade);
 console.log("estou aprendendo javascript")
